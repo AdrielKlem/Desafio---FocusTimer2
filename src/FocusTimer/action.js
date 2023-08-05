242c331831bd0b475fccab81b42e75bf0883b242
Adriel Klem <adrielklem16@gmail.com>
2023-08-05 19:09:06 -0300

    update action

--- src/FocusTimer/action.js
+++ src/FocusTimer/action.js
@@ -1,11 +1,22 @@
 import state from "./state";
 import * as el from "./elements.js"
+import * as timer from './timer.js'
 
 export function toggleRunning() {
-    state.isRunning = el.controls
+    state.isRunning = el.playBtn.classList.toggle("toggleControls")
+    el.pauseBtn.classList.remove("toggleControls")
+
+    if(state.isRunning) {
+        timer.countdown()
+        return
+    } else {
+        reset()
+    }
 }
 
 
 export function reset() {
     state.isRunning = false
+    el.pauseBtn.classList.add("toggleControls")
+    el.playBtn.classList.remove("toggleControls")
 }
