5ff3819ea9df9efc0e1fe8f121a7dd532a3b0883
Adriel Klem <adrielklem16@gmail.com>
2023-08-05 19:05:07 -0300

    news elements

--- src/FocusTimer/action.js
+++ src/FocusTimer/action.js
@@ -1,10 +1,11 @@
 import state from "./state";
+import * as el from "./elements.js"
 
 export function toggleRunning() {
-    
+    state.isRunning = el.controls
 }
 
 
 export function reset() {
-    
+    state.isRunning = false
 }
--- src/FocusTimer/elements.js
+++ src/FocusTimer/elements.js
@@ -2,3 +2,6 @@ export const controls = document.getElementById('controls')
 
 export const minutes = document.getElementById("minutes")
 export const seconds = document.getElementById("seconds")   
+
+export const playBtn = document.getElementById("play")
+export const pauseBtn = document.getElementById("pause")
