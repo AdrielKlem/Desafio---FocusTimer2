af802e7bf11b7cbeff94e3f2ab69eadf91668db2
Adriel Klem <adrielklem16@gmail.com>
2023-08-06 20:44:32 -0300

    fixed bug manipulationTheme

--- src/theme/manipulationTheme.js
+++ src/theme/manipulationTheme.js
@@ -1,10 +1,11 @@
+import * as el from './elements-theme.js'
 import * as sounds from './sounds.js'
 
 function removeAll() {
-    forest.classList.remove('toggleTheme')
-    cafe.classList.remove('toggleTheme')
-    rain.classList.remove('toggleTheme')
-    fireplace.classList.remove('toggleTheme')
+    el.forest.classList.remove('toggleTheme')
+    el.cafe.classList.remove('toggleTheme')
+    el.rain.classList.remove('toggleTheme')
+    el.fireplace.classList.remove('toggleTheme')
 }
 
 export default function changeTheme(isThere, theme, key) {
---
+++ src/volume/volume.js
@@ -0,0 +1,18 @@
+const btnVolumeList = document.querySelectorAll(".volume");
+
+//.volume=this.value
+
+console.log("opa")
+
+// btnVolumeList.forEach(Volume => {
+//     Volume.addEventListener("click", console.log("huumm"))
+// })
+    
+
+
+
+/*
+for(let i of btnVolumeList) {
+    console.log(i)
+}
+*/
